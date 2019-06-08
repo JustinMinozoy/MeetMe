9674d07ea84127db5fda0b56db6136852d0cbc9d
JustinMinozoy <minozoyjustin@gmail.com>
2019-06-08 18:42:55 +0300

    Adding rating activity and help activity

---
+++ app/src/main/java/com/minozoy/magasid/meetme/RatingActivity.java
@@ -0,0 +1,34 @@
+package com.minozoy.magasid.meetme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class RatingActivity extends AppCompatActivity {
+
+    private RatingBar rBar;
+    private TextView tView;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        rBar = (RatingBar) findViewById(R.id.ratingBar1);
+        tView = (TextView) findViewById(R.id.textview1);
+        btn = (Button)findViewById(R.id.btnGet);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int noofstars = rBar.getNumStars();
+                float getrating = rBar.getRating();
+                tView.setText("Rating: "+getrating+"/"+noofstars);
+            }
+        });
+
+    }
+}
