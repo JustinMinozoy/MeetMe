fdd8d52f3a32a1090aa16b0a5fcd1ced4be8895d
JustinMinozoy <minozoyjustin@gmail.com>
2019-06-17 22:18:19 +0300

    adding phone authentication on phone activity

--- app/src/main/java/com/minozoy/magasid/meetme/phoneActivity.java
+++ app/src/main/java/com/minozoy/magasid/meetme/phoneActivity.java
@@ -10,6 +10,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,117 +21,71 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
-public class phoneActivity extends AppCompatActivity {
+public class phoneActivity extends AppCompatActivity  {
+    private EditText editText;
 
-    private Button button,button1;
-    private EditText text1, textCode;
-    FirebaseAuth auth;
-    String codeSent;
+    private CountryCodePicker countryCodePicker;
+    private String TAG;
+    public String verifiCation;
+    private String phoneNumber;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
-        auth = FirebaseAuth.getInstance();
 
-        button = (Button)findViewById(R.id.btn_Login);
-        button1 = findViewById(R.id.btn_very_key);
-        text1 = (EditText)findViewById(R.id.phone);
-        textCode = (EditText)findViewById(R.id.code);
+        editText = (EditText) findViewById(R.id.phoneText);
+        countryCodePicker = (CountryCodePicker) findViewById(R.id.ccp);
+        countryCodePicker.registerCarrierNumberEditText(editText);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.btnproceed).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SIGNIN();
+                String code = countryCodePicker.getFullNumberWithPlus();
+
+
+
+                if (code.isEmpty() || code.length() < 10) {
+                    editText.setError("Invalid Telephone");
+                    editText.requestFocus();
+                    return;
+                }
+
+                String phonenumber = "+" + code ;
+
+                Intent intent = new Intent(phoneActivity.this, VerificationActivity.class);
+                intent.putExtra("phonenumber", phonenumber);
+                startActivity(intent);
+
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                VeryfyCode();
 
-            }
-        });
     }
 
-    private void SIGNIN(){
-        String phoneNumber = text1.getText().toString().trim();
-
-        if(phoneNumber.isEmpty()){
-            text1.setError("Phone Number is Required");
-            text1.requestFocus();
-            return;
-        }
-
-        if(phoneNumber.length() < 10){
-            text1.setError("Phone Number is Incorrect");
-            text1.requestFocus();
-            return;
-        }
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phoneNumber,        // Phone number to verify
-                60,                 // Timeout duration
-                TimeUnit.SECONDS,   // Unit of timeout
-                this,               // Activity (for callback binding)
-                mCallbacks);        // OnVerificationStateChangedCallbacks
-    }
-
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-        @Override
-        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-
-        }
-
-        @Override
-        public void onVerificationFailed(FirebaseException e) {
-
-        }
-
-        @Override
-        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-            super.onCodeSent(s, forceResendingToken);
-
-            codeSent = s;
-        }
-    };
-
-    private void VeryfyCode(){
-
-        String code = textCode.getText().toString().trim();
-        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
-        signInWithPhoneAuthCredential(credential);
-    }
-
-    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Intent intent = new Intent(phoneActivity.this,profiles.class);
-                            startActivity(intent);
-                            finish();
-
-                        } else {
-
-                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                // The verification code entered was invalid
-                            }
-                        }
-                    }
-                });
-    }
-
-    // handle on back pressed go to previous activity
     @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(phoneActivity.this, VerificationActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
     }
+
+
+
 }
